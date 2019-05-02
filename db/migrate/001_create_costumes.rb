4f0d0192967d97ec620da05ce587dae3ae71db01
Amari Ellington <amari.l.ellington@gmail.com>
2019-05-02 16:10:01 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@
 class ClassCostumes < ActiveRecord::Migration[4.2]
 
 def change
-    create_table :costume do |t|
+    create_table costume do |t|
       t.string :name
       t.integer :price
       t.string :size
